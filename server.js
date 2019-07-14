3fba6fcbad9124e56710260cbfca86f0a2fce688
Jeremy Blong <Jeremyablong@icloud.com>
2019-07-13 23:17:36 -0400

    updated production routes heroku

--- server.js
+++ server.js
@@ -14,12 +14,30 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/auth/login", require("./routes/login/auth.js"));
 
-
-
-// index route
-app.get("/", (req, res) => {
-	res.send("API is running");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+app.get('*', function(req, res) {
+  res.sendFile(__dirname, './client/public/index.html')
 })
+
+if (process.env.NODE_ENV === "production") {
+	// Express will serve up production files
+	app.use(express.static("client/build"));
+	// serve up index.html file if it doenst recognize the route
+	app.get('*', function(_, res) {
+	  res.sendFile(__dirname, './client/build/index.html'), function(err) {
+	    if (err) {
+	      res.status(500).send(err)
+	    }
+	  }
+	})
+}; 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
