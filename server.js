4961f5597b56413cde0568a61e7549cec47af4f1
Jeremy Blong <Jeremyablong@icloud.com>
2019-08-06 19:06:04 -0700

    try heroku commit

--- server.js
+++ server.js
@@ -107,47 +107,6 @@ app.listen(PORT, () => {
 })
 
 
-// // When a client connects, we note it in the console
-// io.sockets.on('connection', function (socket) {
-//     console.log('A client is connected!');
-
-// 	const url = "https://"
-
-// 	var options = {
-// 	  "method": "GET",
-// 	  "hostname": "rest.coinapi.io",
-// 	  "Content-Type": "application/json",
-// 	  "path": "/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/latest?period_id=1MIN",
-// 	  "headers": {'X-CoinAPI-Key': '57F960B2-4279-44B7-8474-49F314CF6834'}
-// 	};
-
-
-// 	https.get(options, (resp) => {
-// 	  let data = '';
-
-// 	  // A chunk of data has been recieved.
-// 	  resp.on('data', (chunk) => {
-// 	    data += chunk;
-// 	    console.log(data)
-// 	  });
-
-// 	  // The whole response has been received. Print out the result.
-// 	  // resp.on('end', () => {
-// 	  //   console.log(JSON.parse(data).explanation);
-// 	  // });
-
-// 	}).on("error", (err) => {
-// 	  console.log("Error: " + err.message);
-// 	});
-// });
-
-
-// io.listen(SOCKET_PORT, () => {
-// 	console.log(`Socket.io listening on port ${SOCKET_PORT}`)
-// });
-
-
-
 
 
 
